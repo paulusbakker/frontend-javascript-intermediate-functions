b9253909711b87aba9e9f78001fa2227b6e0653c
paulusbakker <paulusbakker@gmail.com>
2021-07-06 12:38:36 +0200

    opdracht1

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -9,10 +9,18 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 /* 1a: Script schrijven  */
 // De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten namelijk speciale diploma's voor besteld worden.
 // Schrijf de stapjes om dit te kunnen checken eerst uit en vraag jezelf de volgende dingen af:
-// * Hoe kan ik iedere waarde van de array checken op deze conditie?
-// * Hoe zorg ik ervoor dat dit ook werkt wanneer de array 100 entries bevat?
-// * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
+// * Hoe kan ik iedere waarde van de array checken op deze conditie? ->de array doorlopen met een for loop
+// * Hoe zorg ik ervoor dat dit ook werkt wanneer de array 100 entries bevat? ->bepaal de lengte van de array en laat de loop hiernaar doorlopen
+// * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden? ->opslaan in een teller
 // Log het antwoord in de terminal.
+
+// let counter = 0
+// for (let i = 0; i < grades.length; i++) {
+//     if ( grades[i]>= 8) {
+//         counter++
+//     }
+// }
+// console.log(counter)
 
 // ---- Verwachte uitkomst: 6
 
@@ -21,14 +29,24 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
+function cumLaude(grades) {
+    let counter = 0
+    for (let i = 0; i < grades.length; i++) {
+        if ( grades[i]>= 8) {
+            counter++
+        }
+    }
+    console.log(counter)
+}
 
 // ---- Verwachte uitkomsten:
 // cumLaude(grades) geeft 6
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
-
-
+cumLaude(grades)
+cumLaude([6, 4, 5])
+cumLaude([8, 9, 4, 6, 10])
 
 /* Opdracht  2: Gemiddeld cijfer */
 
